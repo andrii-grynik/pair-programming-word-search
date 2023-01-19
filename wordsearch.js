71d0c44fd278f38fe498553b9435d7e25e5c1312
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-01-18 20:18:31 -0700

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,47 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
+    if (word.length < 1 || letters.length === 0) {
+      return false
+    }
+  
+     for(i = 0; i < letters.length; i++) {
+       if(letters[i].length === 0) {
+        return false
+       }
+     }
+  
+    let r = ' '
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+      if (l.includes(word)) {
+        r = true
+      }
     }
-}
-
-module.exports = wordSearch
+  
+    const letters2 = []
+  
+    for (let x = 0; x < letters[0].length; x++) {
+      const submatrix = []
+      letters2.push(submatrix)
+    }
+    for (i = 0; i < letters.length; i++) {
+      for (let j = 0; j < letters[0].length; j++) {
+        letters2[j][i] = letters[i][j]
+      }
+    }
+  
+    const verticalJoin = letters2.map(ls => ls.join(''))
+    for (l of verticalJoin) {
+      if (l.includes(word)) {
+        r = true
+      }
+    }
+  
+    if (r !== true) {
+      r = false
+    }
+  
+    return r
+  }
+  
+  module.exports = wordSearch
+  
